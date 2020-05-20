037cc583a4d69b1a0d74d09397b3405186eff658
CleoGitHub <survcleo@hotmail.fr>
2020-05-20 16:24:07 +0200

    mis a jour

--- View/web/src/store.js
+++ View/web/src/store.js
@@ -24,7 +24,7 @@ export default new Vuex.Store({
         // animationOnGoing: false,
         // playersAIStatus:null,
         animationIAEnCours:false,
-        tutoEnCours: true,
+        tutoEnCours: false,
         jaiCompris: false
     },
     mutations: {
