a855760a5a05c0d799080d15e239236d636c09ff
CleoGitHub <survcleo@hotmail.fr>
2020-05-20 22:05:06 +0200

    Lancement App

--- src/main/java/Azul.java
+++ src/main/java/Azul.java
@@ -1,11 +1,21 @@
-import java.security.InvalidParameterException;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 import Controller.Controller;
 import Controller.Server.JavaHTTPServerLauncher;
 
 public class Azul {
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		Controller controller = Controller.getInstance();
 		JavaHTTPServerLauncher.start();
+
+		File htmlFile = new File("dist/index.html");
+
+		try {
+			Desktop.getDesktop().browse(htmlFile.toURI());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
