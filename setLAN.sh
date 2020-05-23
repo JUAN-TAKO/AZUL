5cd73911a5d69181148598e2075adeb91295da2c
Dimitri Cross <dimitri.cross@etu.univ-grenoble-alpes.fr>
2020-05-23 19:07:56 +0200

    setLAN no longer clean itself

--- setLAN.sh
+++ setLAN.sh
@@ -1,14 +1,16 @@
-PROJECT_DIR=/home/cd/Documents/Java/AZUL
+PROJECT_DIR=.
 YOUR_IP=192.168.0.10
 TCPPORT=9958
 
 shopt -s globstar
-for f in $PROJECT_DIR/**
+for f in $PROJECT_DIR/src/** $PROJECT_DIR/out/**
 do
-	[ -f $f ] && grep -q 192.168.0.10 $f && mv $f $f.tmp && sed s/192.168.0.10/$YOUR_IP/g $f.tmp >$f && rm $f.tmp && echo $f cleaned.
+	[ -f $f ] && grep -q localhost $f && mv $f $f.tmp && sed s/localhost/$YOUR_IP/g $f.tmp >$f && rm $f.tmp && echo $f cleaned.
 done
 
 cd $PROJECT_DIR/out/artifacts/App_jar/dist
 sed 's/<\/body/<script src=js\/autorefresh.js><\/script><\/body/' index.html >indexLAN.html && echo indexLAN.html created.
 
+echo Starting Python server...
+echo AZUL can now be accessed in LAN at $YOUR_IP:$TCPPORT/indexLAN.html
 python3 -m http.server $TCPPORT
