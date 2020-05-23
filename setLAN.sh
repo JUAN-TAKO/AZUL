8a527ddfe64052c785ca80b0ab5f8a16f7e3cb58
Dimitri Cross <dimitri.cross@etu.univ-grenoble-alpes.fr>
2020-05-23 19:12:57 +0200

    setLAN now only cleans View part (Vue and js)

--- setLAN.sh
+++ setLAN.sh
@@ -3,7 +3,7 @@ YOUR_IP=192.168.0.10
 TCPPORT=9958
 
 shopt -s globstar
-for f in $PROJECT_DIR/src/** $PROJECT_DIR/out/**
+for f in $PROJECT_DIR/src/main/java/View/** $PROJECT_DIR/out/**
 do
 	[ -f $f ] && grep -q localhost $f && mv $f $f.tmp && sed s/localhost/$YOUR_IP/g $f.tmp >$f && rm $f.tmp && echo $f cleaned.
 done
@@ -11,6 +11,6 @@ done
 cd $PROJECT_DIR/out/artifacts/App_jar/dist
 sed 's/<\/body/<script src=js\/autorefresh.js><\/script><\/body/' index.html >indexLAN.html && echo indexLAN.html created.
 
-echo Starting Python server...
+echo
 echo AZUL can now be accessed in LAN at $YOUR_IP:$TCPPORT/indexLAN.html
 python3 -m http.server $TCPPORT
